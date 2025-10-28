38b18c56e84147591134d082f4c47fa5a727ad4a
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2025-10-28 23:30:36 +0530

    Time: 4 ms (68.35%) | Memory: 21.9 MB (80.73%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,95 +1,25 @@
 class Solution {
 public:
-    // bool isValidR(int curr, vector<int> nums) {
-    //     int n = nums.size();
-    //     if (curr < 0 || curr >= n) {
-    //         for (int i = 0; i < n; i++)
-    //             if (nums[i] != 0) return false;
-    //         return true;
-    //     }
-
-    //     curr++;
-    //     if (curr >= n) {
-    //         for (int i = 0; i < n; i++)
-    //             if (nums[i] != 0) return false;
-    //         return true;
-    //     }
-
-    //     if (nums[curr] == 0)
-    //         return isValidR(curr, nums);
-    //     else {
-    //         nums[curr]--;
-    //         return isValidL(curr, nums);
-    //     }
-    // }
-
-    // bool isValidL(int curr, vector<int> nums) {
-    //     int n = nums.size();
-    //     if (curr < 0 || curr >= n) {
-    //         for (int i = 0; i < n; i++)
-    //             if (nums[i] != 0) return false;
-    //         return true;
-    //     }
-
-    //     curr--;
-    //     if (curr < 0) {
-    //         for (int i = 0; i < n; i++)
-    //             if (nums[i] != 0) return false;
-    //         return true;
-    //     }
-
-    //     if (nums[curr] == 0)
-    //         return isValidL(curr, nums);
-    //     else {
-    //         nums[curr]--;
-    //         return isValidR(curr, nums);
-    //     }
-    // }
-
-    bool check(vector<int>& nums , int i , int count , int dir){  int idx=i;
-        vector<int>temp=nums;
-        while(idx>=0 && idx<nums.size() && count>0){
-            if(temp[idx]>0){
-              temp[idx]--;
-              dir*=-1;
-               if(temp[idx]==0){
-                count--;
-
-            }
-              }
-              
-            idx+=dir;
+    int sum(vector<int>& nums , int i , int j){
+        int count=0;
+        for(int k=i;k<=j;k++){
+            count+=nums[k];
         }
-        return count ==0;
+        return count;
     }
-
     int countValidSelections(vector<int>& nums) {
-        // int n = nums.size();
-        // int count = 0;
-        // for (int i = 0; i < n; i++) {
-        //     if (nums[i] == 0) {
-        //         if (isValidR(i, nums)) count++;
-        //         if (isValidL(i, nums)) count++;
-        //     }
-        // }
-        // return count;
-
         int n=nums.size();
         int result=0;
-        int count=0;
         for(int i=0;i<n;i++){
-            if(nums[i]!=0) count++;
+            if(nums[i]==0){
+                
+                int sumL=sum(nums , 0 , i-1);
+                int sumR=sum(nums , i+1 , n-1);
+                if(sumL==sumR) result+=2;
+                if(abs(sumL-sumR)==1) result+=1;
+            }
         }
+        return result;
         
-        for(int i=0;i<n;i++){
-           if(nums[i]==0){
-             if(check(nums , i , count , -1)) result++;
-            if(check(nums , i , count , +1)) result++;
-           }
-
-
-        }
-
-      return result;
     }
 };
