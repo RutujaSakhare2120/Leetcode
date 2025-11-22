3ba40e2fdbb0444a717fb4ff4601cf2978fa430c
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2025-11-22 18:42:41 +0530

    Time: 11 ms (15.05%) | Memory: 24.8 MB (18.68%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,26 @@
+bool cmp(vector<int>&v1 , vector<int>&v2){
+ return v1[1]<v2[1];
+}
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //sort array in ascending order on the basis of second point
+        sort(intervals.begin() , intervals.end() , cmp);
+        vector<vector<int>> result;
+        int n=intervals.size();
+        result.push_back(intervals[n-1]);
+        for(int i=n-2;i>=0;i--){
+            vector<int>curr=intervals[i];
+            if(curr[1]>=result[result.size()-1][0]){
+               // first.end>=second.start overlap
+
+               result[result.size()-1][0]=min(curr[0] ,result[result.size()-1][0] );
+               result[result.size()-1][1]=max(curr[1] , result[result.size()-1][1]);
+            }
+            else{
+               result.push_back(intervals[i]);
+            }
+        }
+        return result;
+    }
+};
