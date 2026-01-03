2a48cf7b22755267de3ff7a7230238bb3ea31887
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2026-01-03 17:54:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (50.39%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,16 +1,21 @@
 class Solution {
-public:
-    int helper(int sr, int sc , int er , int ec,vector<vector<int>> &dp){
-        if(sr==er && sc==ec) return 1;
-        if(sr>er || sc>ec) return 0;
-        if(dp[sr][sc]!=-1) return dp[sr][sc];
-        int rightways=helper(sr,sc+1,er,ec,dp);
-        int downways=helper(sr+1,sc,er,ec,dp);
-        return dp[sr][sc]=rightways+downways;
-    }
+public://tabulation
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-       return helper(0,0,m-1,n-1,dp);
-        
+        for(int i=0;i<m;i++){
+          dp[i][0]=1;
+        }
+         for(int j=0;j<n;j++){
+          dp[0][j]=1;
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+
+
+      return dp[m-1][n-1];
     }
 };
