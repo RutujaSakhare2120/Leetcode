7532f4511f06ff58ba27c173cefd8e0d92dbbeca
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2026-01-05 20:15:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (34.74%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-   //recusion
-
-   int helper(int n, vector<int>&dp){
-     if(n==0) return 0;
-    else  if(n==1 || n==2) return 1;
-    if(dp[n]!=-1) return dp[n];
-     return dp[n]=helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
-    
-   }
+//tabulation
     int tribonacci(int n) {
-        vector<int>dp(n+1,-1);
-        int ans= helper(n,dp);
-        return ans;
+        vector<int>dp(n+1);
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+       return  dp[n];
     }
 };
