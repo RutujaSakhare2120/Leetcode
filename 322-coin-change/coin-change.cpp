9f513bac2c44f1420abe6ff1c04854de483f3e67
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2026-01-08 19:17:15 +0530

    Time: 62 ms (12.14%) | Memory: 50 MB (17.28%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    //recursion
+    
     int helper(vector<int>& coins, int amount , int idx){
       
-        if(idx==coins.size()) {
+        if(idx<0) {
               if(amount==0) return 0;
               else  return INT_MAX;//no valid ans
         }
         if(dp[idx][amount]!=-1) return dp[idx][amount];
-        if(amount-coins[idx]<0) return dp[idx][amount]=helper(coins,amount,idx+1);//leave
+        if(amount-coins[idx]<0) return dp[idx][amount]=helper(coins,amount,idx-1);//leave
         long long take=1LL+helper(coins,amount-coins[idx],idx);
-        long long leave=helper(coins,amount,idx+1);
+        long long leave=helper(coins,amount,idx-1);
         return dp[idx][amount]=min(take , leave);
     }
     int coinChange(vector<int>& coins, int amount) {
-        dp.resize(coins.size() , vector<int>(amount+1,-1));
-        int ans= helper(coins,amount,0);
+        int n=coins.size();
+        dp.resize( n, vector<int>(amount+1,-1));
+        int ans= helper(coins,amount,n-1);
         if(ans==INT_MAX) return -1;
         return ans;
     }
