13f79fd8ed085ec0c104290166f6ed8956ed50c9
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2026-01-08 16:45:20 +0530

    Time: 115 ms (39.72%) | Memory: 59.7 MB (10.15%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
+   unordered_map< int,vector<int> >dp;
     int total=0;
     int helper(vector<int>& nums,int idx , int sum ,int target)
     {    if(idx==nums.size()){
             if(sum==target) return 1;
             else return 0;
           }
-          if( dp[idx][sum+total]!=-1) return dp[idx][sum+total];
+          if( dp[sum][idx]!=-1) return dp[sum][idx];
          int add=helper(nums, idx+1, sum+nums[idx] , target);
          int sub=helper(nums, idx+1, sum-nums[idx] , target);    
-         return  dp[idx][sum+total]=add+sub;
+         return   dp[sum][idx]=add+sub;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         for(int a:nums) total+=a;
-        dp.resize(n,vector<int>(2*total+1,-1));
+        for(int i=-total;i<=total;i++){
+            vector<int>v(nums.size(),-1);
+            dp[i]=v;
+        }
         return helper(nums, 0 , 0 ,target);
     }
 };
