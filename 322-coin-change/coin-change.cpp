dc6d502231102c94ca5d2cf1c829f0176060249f
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2026-01-10 15:47:43 +0530

    Time: 54 ms (18.95%) | Memory: 53.1 MB (10.84%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -3,23 +3,27 @@ public:
 //tabulation
     vector<vector<int>>dp;
     
-    int helper(vector<int>& coins, int amount , int idx){
-      
-        if(idx<0) {
-              if(amount==0) return 0;
-              else  return INT_MAX;//no valid ans
-        }
-        if(dp[idx][amount]!=-1) return dp[idx][amount];
-        if(amount-coins[idx]<0) return dp[idx][amount]=helper(coins,amount,idx-1);//leave
-        long long take=1LL+helper(coins,amount-coins[idx],idx);
-        long long leave=helper(coins,amount,idx-1);
-        return dp[idx][amount]=min(take , leave);
-    }
+    
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        dp.resize( n, vector<int>(amount+1,-1));
-        int ans= helper(coins,amount,n-1);
-        if(ans==INT_MAX) return -1;
-        return ans;
+        dp.resize( n+1, vector<int>(amount+1,INT_MAX));
+         for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+         }
+
+         for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+
+                if(j-coins[i-1]<0)  dp[i][j]=dp[i-1][j];
+                else{
+                long long take=1LL+dp[i][j-coins[i-1]];
+                long long leave=dp[i-1][j];
+                 dp[i][j]=min(take , leave);
+                }
+                
+            }
+         }
+        if(dp[n][amount]==INT_MAX) return -1;
+        return dp[n][amount];
     }
 };
