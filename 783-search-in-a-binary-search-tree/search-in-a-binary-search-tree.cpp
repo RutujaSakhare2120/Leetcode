058fcad5410d22d804c0477dbdf112fc2d28ec4a
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2026-01-14 23:54:28 +0530

    Time: 0 ms (100.00%) | Memory: 35.4 MB (92.79%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -11,16 +11,12 @@
  */
 class Solution {
 public:
-     TreeNode* helper(TreeNode* root, int val){
-        if(!root)  return NULL;
-        if(root->val==val) return root;
-        if(root->val>val) return helper(root->left , val);
-        return helper(root->right , val);
-        
-     }
+     
     TreeNode* searchBST(TreeNode* root, int val) {
      if(!root)  return NULL;
-     return helper(root , val);
-        
+     if(root->val==val) return root;
+     if(root->val>val) return searchBST(root->left , val);
+     else return searchBST(root->right , val);
+     
     }
 };
