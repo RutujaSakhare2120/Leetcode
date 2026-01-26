cceae7da7c38f2732d5edb4c362565aa12eb22dd
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2026-01-26 14:52:13 +0530

    Time: 32 ms (93.02%) | Memory: 45.4 MB (69.50%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
+//if all numbers are positive then this code is correct
+    
     int subarraySum(vector<int>& nums, int k) {
+        //prefix sum
         int n=nums.size();
-        int count=0;
-        for(int i=0;i<n;i++){
-            int ans=0;
-            for(int j=i;j<n;j++){
-               ans+=nums[j];
-               if(ans==k){
-                count++;
-                continue;
-               }
-            }
+        for(int i=1;i<n;i++){
+            nums[i]+=nums[i-1];
         }
-      return count;
+        
+        int count=0;
+        unordered_map<int ,int>s;
+        for(int i=0;i<n;i++){
+            
+            if(nums[i]==k){
+                count++;
+               
+            }
+          
+                int n1=nums[i]-k;
+                if(s.find(n1)!=s.end()){
+                    count+=s[n1];
+                }
+           
+            
+             s[nums[i]]++;
+
+        }
+         return count;
+        
+        
     }
 };
