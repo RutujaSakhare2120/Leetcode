b6ace189b6e661e17c31cba755f5223a0e4124f6
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2026-01-30 14:42:16 +0530

    Time: 23 ms (87.41%) | Memory: 97.7 MB (88.79%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
-    long long minimumCost(string source, string target,
-                          vector<char>& original,
-                          vector<char>& changed,
-                          vector<int>& cost) {
-
-        const long long INF = 1e18;
-
-        // STEP 1: build cost graph for characters
-        vector<vector<long long>> dist(26, vector<long long>(26, INF));
-
-        for(int i = 0; i < 26; i++)
-            dist[i][i] = 0;
-
-        for(int i = 0; i < original.size(); i++){
-            int u = original[i] - 'a';
-            int v = changed[i] - 'a';
-            dist[u][v] = min(dist[u][v], (long long)cost[i]);
+    const int INF = 1e9;        // for int
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<int>>grid(26 ,vector<int>(26,INF) );
+        for(int i=0;i<26;i++){
+            grid[i][i]=0;
         }
 
-        // STEP 2: Floyd-Warshall (minimum cost between all chars)
-        for(int k = 0; k < 26; k++){
-            for(int i = 0; i < 26; i++){
-                for(int j = 0; j < 26; j++){
-                    if(dist[i][k] + dist[k][j] < dist[i][j]){
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
+        for(int i=0;i<original.size();i++){
+            char src=original[i];
+            char dest=changed[i];
+            int a=src-'a';
+            int b=dest-'a';
+            grid[a][b]=min( grid[a][b] ,cost[i] );
+        }
+
+        //apply floyd warshall algorithm
+
+        for(int k=0;k<26;k++){
+            for(int i=0;i<26;i++){
+                for(int j=0;j<26;j++){
+                    if(grid[i][k] < INF && grid[k][j] < INF)
+    grid[i][j] = min(grid[i][j], grid[i][k] + grid[k][j]);
+
                 }
             }
         }
-
-        // STEP 3: calculate answer (your loop logic, corrected)
-        long long ans = 0;
-        for(int i = 0; i < source.size(); i++){
-            if(source[i] == target[i]) continue;
-
-            long long d = dist[source[i]-'a'][target[i]-'a'];
-            if(d == INF) return -1;   // impossible
-
-            ans += d;
+       long long ans=0;
+        for(int i=0;i<source.size();i++){
+            char src=source[i];
+            char dest=target[i];
+            int a=src-'a';
+            int b=dest-'a';
+            if(grid[a][b]==INF) return -1;
+            ans+= (long long)grid[a][b];
         }
-
         return ans;
     }
 };
