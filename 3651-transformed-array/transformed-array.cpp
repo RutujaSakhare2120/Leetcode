42e11b716ea362b2b8509d48725ed2886f37cf5b
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2026-02-05 12:44:17 +0530

    Time: 7 ms (54.87%) | Memory: 25.8 MB (39.55%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -4,33 +4,10 @@ public:
         int n=nums.size();
          vector<int> result(n);
         for(int i=0;i<n;i++){
-            int a=nums[i];
-            if(a==0){
-             result[i]=nums[i];
-            }
-            else if(a>0){
-               int j=i;
-               for(int k=0;k<a;k++){
-                 j++;
-                 if(j==n){
-                    j=0;
-                 }
-               }
-               result[i]=nums[j];
-            }
-            else{
-                a=-1*a;
-                int j=i;
-               for(int k=0;k<a;k++){
-                 j--;
-                 if(j<0){
-                    j=n-1;
-                 }
-               }
-               result[i]=nums[j];
-            }
-
-            
+         nums[i]%n;
+         int newIdx=(i+nums[i])%n;
+         if(newIdx<0) newIdx=newIdx+n;
+         result[i]=nums[newIdx];
         }
         return result;
         
