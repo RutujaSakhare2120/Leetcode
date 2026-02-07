a0a0d4d12a7dd7fe6c83a16be5315a7c96264e8b
RutujaSakhare2120 <rutujasakhare098@gmail.com>
2026-02-07 15:22:27 +0530

    Time: 63 ms (12.28%) | Memory: 55.2 MB (16.57%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n=s.length();
-        stack<char>st;
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(st.empty()) st.push(s[i]); 
-            else if(s[i]=='a' && st.top()=='a') st.push(s[i]);
-            else if(s[i]=='b' && st.top()=='b') st.push(s[i]);
-            else if(s[i]=='b' && st.top()=='a') st.push(s[i]);
-            else if(s[i]=='a' && st.top()=='b') {
-                st.pop();
-                count++;
-            }
+        int n = s.length();
+        if (n <= 1) return 0;
 
+        vector<int> leftB(n), rightA(n);
+
+        int b = 0;
+        for (int i = 0; i < n; i++) {
+            leftB[i] = b;          // b's before i
+            if (s[i] == 'b') b++;
         }
+
+        int a = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            rightA[i] = a;         // a's after i
+            if (s[i] == 'a') a++;
+        }
+
+        int count = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            count = min(count, leftB[i] + rightA[i]);
+        }
+
         return count;
-        
     }
 };
